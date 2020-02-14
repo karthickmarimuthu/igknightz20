02100bcd9a04a396e821c6a0743f0c8597c73edf
karthickmarimuthu <42971271+karthickmarimuthu@users.noreply.github.com>
2020-02-14 22:50:08 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -324,7 +324,7 @@
 
 function makeTimer() {
 
-		var endTime = new Date("19 February 2020 10:00:00 GMT+01:00");			
+		var endTime = new Date("19 February 2020 09:30:00");			
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
